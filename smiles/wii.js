df4b2ebc2d4b109d336b7d49d00fd149b565f907
wtaype <wilder.taype16@gmail.com>
2026-02-01 18:04:10 -0500

    Actualizacion Principal v12.10.11

--- smiles/wii.js
+++ smiles/wii.js
@@ -9,10 +9,10 @@ export let link = 'https://wtaype.github.io/';
 export let version = 'v12';
 
 /** Actualizar main luego esto, pero si es mucho, solo esto. (1)
-git tag v12 -m "Version v12" ; git push origin v12
+git tag v12 -m "Version v12" ; git push origin v12 
 
 //  ACTUALIZACIÓN PRINCIPAL ONE DEV [START] (2)
-git add . ; git commit -m "Actualizacion Principal v12.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v12.10.10" ; git push origin main 
 
 // En caso de emergencia, para actualizar el Tag existente. (3)
 git tag -d v12 ; git tag v12 -m "Version v12 actualizada" ; git push origin v12 --force
