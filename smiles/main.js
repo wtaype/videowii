e090e3fced2353e775bfe48575319f235256150f
wtaype <wilder.taype16@gmail.com>
2026-02-17 19:19:18 -0500

    Actualizacion Principal v14.10.10

--- smiles/main.js
+++ smiles/main.js
@@ -4,7 +4,7 @@ import { getls } from './widev.js';
 import { rutas } from './rutas/ruta.js';
 import './header.js';
 
-const pages = ['inicio','preview','optimizar','editar','online','conversor','extraer','acerca'];
+const pages = ['inicio','extraer','preview','optimizar','editar','online','conversor','acerca'];
 pages.forEach(pg => rutas.register(`/${pg}`, () => import(`./web/${pg}.js`))); // Publico general
 
 rutas.register('/smile',() => getls('wiSmile')?import('./smile/smile.js'):import('./smile/descubre.js'));//Con Auth
